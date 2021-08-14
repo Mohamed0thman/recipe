0579ff81b80c7234f8dbaae066018b4489a3b979
Mohamed0thman <60795079+Mohamed0thman@users.noreply.github.com>
2021-08-14 04:46:48 +0200

    new

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import Skeletonfrom from "../../components/Skeleton";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
