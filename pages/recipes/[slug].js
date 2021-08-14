22d8a8d424ff4ec2a18835c3b4bb807adcc1b745
Mohamed0thman <60795079+Mohamed0thman@users.noreply.github.com>
2021-08-14 04:43:11 +0200

    new

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeletonfrom from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -32,13 +33,13 @@ export const getStaticProps = async ({ params }) => {
 
   return {
     props: { recipe: items[0] },
+    revalidate: 1,
   };
 };
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
   const { thumbnail, title, cookingTime, ingredients, method } = recipe.fields;
-  console.log(method);
-  console.log(recipe);
   return (
     <div>
       <div className="banner">
